6b0655011a6e47744949ef32d0c4e70f88f21bef
Dan Colmenares <dacolmenares93@gmail.com>
2021-11-24 21:27:26 -0500

    Advanced React TypeScript testing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,13 @@ export class Navbar extends React.Component<{ user: User | undefined }> {
 
     if (this.props.user) {
       loginLogOut = (
-        <Link to="/login" style={{ float: "right" }}>
+        <Link to="/logout" data-testid="logout-link" style={{ float: "right" }}>
           {this.props.user.userName}
         </Link>
       );
     } else {
       loginLogOut = (
-        <Link to="/login" style={{ float: "right" }}>
+        <Link to="/login" data-testid="login-link" style={{ float: "right" }}>
           Login
         </Link>
       );
@@ -22,9 +22,15 @@ export class Navbar extends React.Component<{ user: User | undefined }> {
 
     return (
       <div className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/spaces">Spaces</Link>
+        <Link data-testid="home-link" to="/">
+          Home
+        </Link>
+        <Link data-testid="profile-link" to="/profile">
+          Profile
+        </Link>
+        <Link data-testid="spaces-link" to="/spaces">
+          Spaces
+        </Link>
         {loginLogOut}
       </div>
     );
